9b43f90de6fc87c905e72b5d65ca6543a9e85d12
鈴木 直登 <nsuzuki7713@gmail.com>
2018-12-23 21:08:01 +0900

    Word Cloudで可視化

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 ## ライブラリインストール
 
 ```
-# https://qiita.com/bakira/items/00743d10ec42993f85eb
 pip install requests requests-oauthlib
+pip install janome
+pip install wordcloud
+pip install emoji --upgrade
 ```
 
 ## config.py.example の修正
@@ -12,6 +14,15 @@ pip install requests requests-oauthlib
 ファイルをコピーをして、ファイル名を config.py にする
 twitter api のトークンを設定する
 
+## 使い方
+
+1. params の screen_name にアカウント名を入れる（getTimelines2.py）
+2. `python getTimeLines2.py`を実行し、ツイートを取得(output/tweet_data に出力される)
+3. `python wordcloud.py` を実行し、Word Cloud の画像を出力する(wordcloud_sample.png)
+   ※除外したい単語は counter 関数の中に記載する
+
 ## 参考 URL
 
 [Python で Twitter API を利用していろいろ遊んでみる](https://qiita.com/bakira/items/00743d10ec42993f85eb)
+[【Python でテキストマイニング】Twitter データを WordCloud で可視化してみる](http://www.randpy.tokyo/entry/python_wordcloud)
+[python で絵文字を駆逐する](https://qiita.com/yoshimo123/items/85331d881aed9ad41020)
---
+++ getTimelines2.py
@@ -0,0 +1,51 @@
+import json
+import config
+from requests_oauthlib import OAuth1Session
+from time import sleep
+import emoji
+
+# 絵文字を除去する
+def remove_emoji(src_str):
+    return ''.join(c for c in src_str if c not in emoji.UNICODE_EMOJI)
+
+# APIキー設定
+CK = config.CONSUMER_KEY
+CS = config.CONSUMER_SECRET
+AT = config.ACCESS_TOKEN
+ATS = config.ACCESS_TOKEN_SECRET
+
+# 認証処理
+twitter = OAuth1Session(CK, CS, AT, ATS)  
+
+# タイムライン取得エンドポイント
+url = "https://api.twitter.com/1.1/statuses/user_timeline.json"  
+
+# パラメータの定義
+params = {'screen_name': 'naoto_7713',
+          'exclude_replies': True,
+          'include_rts': False,
+          'count': 200}
+
+f_out = open('./output/tweet_data', 'w')
+
+for j in range(100):
+    res = twitter.get(url, params=params)
+    if res.status_code == 200:
+        # API残り回数
+        limit = res.headers['x-rate-limit-remaining']
+        print("API remain: " + limit)
+        if limit == 1:
+            sleep(60*15)
+
+        n = 0
+        timeline = json.loads(res.text)
+        # 各ツイートの本文を表示
+        for i in range(len(timeline)):
+            if i != len(timeline)-1:
+                f_out.write(remove_emoji(timeline[i]['text']) + '\n')
+            else:
+                # 一番最後のツイートIDをパラメータmax_idに追加
+                f_out.write(remove_emoji(timeline[i]['text']) + '\n')
+                params['max_id'] = timeline[i]['id']-1
+
+f_out.close()
